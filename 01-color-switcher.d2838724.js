2d8228df79221c1dbd17cd878b185ff3bb0ed6d3
Ivanka077 <122215710+Ivanka077@users.noreply.github.com>
2023-06-15 15:42:11 +0000

    Deploying to gh-pages from @ Ivanka077/goit-js-hw-09@2f857bc50129afe618ca306c041a9975b427c8a2 🚀

---
+++ 01-color-switcher.d2838724.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]");document.querySelector("[data-stop]"),document.body,active;t.addEventListener("click",(function(t){setInterval((function(){"#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)}))}();
+//# sourceMappingURL=01-color-switcher.d2838724.js.map
