b6bc239bbedab7398ceca798a589e824a1be8485
Ivanka077 <122215710+Ivanka077@users.noreply.github.com>
2023-06-16 20:33:26 +0000

    Deploying to gh-pages from @ Ivanka077/goit-js-hw-09@dcfd64718e19ef729541bf802f9a607707e1e984 🚀

---
+++ 01-color-switcher.635b9160.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let n=null;t.addEventListener("click",(function(){t.disabled=!0,n=setInterval((()=>{!function(){const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}()}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.635b9160.js.map
--- 01-color-switcher.7d5e52f8.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]");document.querySelector("[data-stop]"),document.body,active;t.addEventListener("click",(function(t){setInterval((()=>{Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}),1e3)}));
-//# sourceMappingURL=01-color-switcher.7d5e52f8.js.map
