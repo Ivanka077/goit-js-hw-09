dcfd64718e19ef729541bf802f9a607707e1e984
Ivanka077 <122215710+Ivanka077@users.noreply.github.com>
2023-06-16 23:32:59 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,41 +1,31 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
-const bodyEl = document.body;
 
-const dddS = active;
+
 const NOTIFICATION_DELAY = 1000;
+let intervalId = null;
 
-startBtn.addEventListener('click', changeBodeColor);
+startBtn.addEventListener('click', startChangingBodyColor);
+stopBtn.addEventListener('click', stopChangingBodyColor);
 
-function changeBodeColor(evt) {  
-    setInterval(() => {
-      getRandomHexColor()  
-    }, NOTIFICATION_DELAY);
+function startChangingBodyColor() {
+  startBtn.disabled = true;
+  intervalId = setInterval(() => {
+    changeBodyColor(); 
+  }, NOTIFICATION_DELAY);
+}
+
+function changeBodyColor() {  
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+}
+
+function stopChangingBodyColor() {
+  startBtn.disabled = false;
+  clearInterval(intervalId);
 }
 
 
-
-
 function getRandomHexColor() {
-   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
- }
-
-
-
-
-
-// HTML містить кнопки «Start» і «Stop».
-
-// <button type="button" data-start>Start</button>
-// <button type="button" data-stop>Stop</button>
-
-// Напиши скрипт, який після натискання кнопки «Start», раз на секунду змінює колір фону <body> на випадкове значення, використовуючи інлайн стиль. Натисканням на кнопку «Stop» зміна кольору фону повинна зупинятися.
-
-// УВАГА
-// Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів. Зроби так, щоб доки зміна теми запущена, кнопка «Start» була неактивною (disabled).
-
-// Для генерування випадкового кольору використовуй функцію getRandomHexColor.
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-// }
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
